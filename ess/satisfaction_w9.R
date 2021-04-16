e710a5cab7f0e8be405cd0f251506e2f73dae648
obbergeron <42869448+aka09@users.noreply.github.com>
2021-04-16 13:29:21 -0400

    csdc presentation

--- ess/satisfaction_w9.R
+++ ess/satisfaction_w9.R
@@ -9,6 +9,7 @@ source("functions/confint.R")
 
 # Data from 9th ESS wave
 ess9.orig <- read_dta("ess/data/ess_wave9.dta")
+load("cleaned_data/ess_clean.RData")
 
 # Renaming columns
 ess9 <- 
@@ -91,3 +92,53 @@ ess9 %>%
   scale_x_continuous(breaks = seq(20, 90, 10)) +
   scale_y_continuous(breaks = seq(0, .1, .02)) +
   theme_bw(base_family = "Fira Sans")
+
+# by age and country
+ess_cum %>% 
+  filter(age >= 18 & age <= 75) %>% 
+  filter(dem_satisfied < 11) %>% 
+  group_by(age, country) %>% 
+  summarise(mean_satisfied = weighted.mean(dem_satisfied, 
+                                           na.rm = T,
+                                           w = pspwght),
+            lwr = lwr_conf(dem_satisfied),
+            upr = upr_conf(dem_satisfied),
+            `Sample size` = n()) %>% 
+  ggplot(aes(x = age, y = mean_satisfied, weight = `Sample size`)) +
+  geom_point(aes(size = `Sample size`), alpha = 0.2) +
+  geom_smooth(se = FALSE) +
+  labs(x = "Age",
+       y = "Satisfaction with democracy (0-10)") +
+  scale_x_continuous(breaks = seq(20, 90, 10)) +
+  theme_bw(base_family = "Fira Sans",
+           base_size = 12) +
+  guides(size = FALSE) +
+  facet_wrap(~country)
+
+ggsave("ess/satisfaction_age_country.png", height = 10, width = 10)
+
+# Centered around 0 (0 is country-level mean)
+ess_cum %>% 
+  filter(age >= 18 & age <= 75) %>% 
+  filter(dem_satisfied < 11) %>% 
+  group_by(country) %>% 
+  mutate(satisfied_diff = dem_satisfied - mean(dem_satisfied, na.rm = T)) %>% 
+  group_by(age, country) %>% 
+  summarise(mean_satisfied = weighted.mean(satisfied_diff, 
+                                           na.rm = T,
+                                           w = pspwght),
+            lwr = lwr_conf(satisfied_diff),
+            upr = upr_conf(satisfied_diff),
+            `Sample size` = n()) %>% 
+  ggplot(aes(x = age, y = mean_satisfied, weight = `Sample size`)) +
+  geom_point(aes(size = `Sample size`), alpha = 0.2) +
+  geom_smooth(se = FALSE) +
+  labs(x = "Age",
+       y = "Satisfaction with democracy (0-10)") +
+  scale_x_continuous(breaks = seq(20, 90, 10)) +
+  theme_bw(base_family = "Fira Sans",
+           base_size = 12) +
+  guides(size = FALSE) +
+  facet_wrap(~country)
+
+ggsave("ess/satisfaction_age_country(diff).png", height = 10, width = 10)
